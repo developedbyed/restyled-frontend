f4cc53eb68420dc0f39c685b69b6052dc037c915
developedbyed <ed.magician@gmail.com>
2023-09-07 17:09:28 +0100

    disabled product-gallery

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import TodoList from "@/components/TodoList"
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ProductGallery />
+      {/* <ProductGallery /> */}
       <TodoList />
     </main>
   )
